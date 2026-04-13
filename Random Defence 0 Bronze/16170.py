ede211c2f852e6735509ec6634370455955e2090
YEOM <junhayeom@gmail.com>
2026-04-13 20:14:06 +0900

    260413

---
+++ Random Defence 0 Bronze/16170.py
@@ -0,0 +1,40 @@
+# Authored by : marigold2003
+# Date : 2026-04-13
+# Link : https://www.acmicpc.net/problem/16170
+
+
+import sys
+
+input = sys.stdin.readline
+
+
+# [Summary] 오늘의 날짜는?
+
+# utc+0 기준 현재 연월일을 출력하시오.
+
+
+def main() -> None:
+
+    # [Ideas]
+
+    # 지금이 26.04.13 20시니까
+    # 9시간 느린게 utc+0이지. 날짜는 안바뀌네
+
+    ##########
+
+    print(2026)
+    print("04")
+    print(13)
+
+    ##########
+
+    return
+
+
+# [Review]
+
+# 그것이 오늘이니까
+
+
+if __name__ == "__main__":
+    main()
