68f0c88f9cff7a5f1ae10ee65b003dc0cde6e7f1
YEOM <junhayeom@gmail.com>
2026-04-18 22:49:01 +0900

    260418

---
+++ Random Defence 0 Bronze/32951.py
@@ -0,0 +1,38 @@
+# Authored by : marigold2003
+# Date : 2026-04-18
+# Link : https://www.acmicpc.net/problem/32951
+
+
+import sys
+
+input = sys.stdin.readline
+
+
+# [Summary] AI 선도대학
+
+# N년은 2024년으로부터 몇 년이나 지난 년일까?
+
+
+def main() -> None:
+
+    # [Ideas]
+
+    # 2024를 빼자.
+
+    ##########
+
+    N = int(input())
+    print(N - 2024)
+
+    ##########
+
+    return
+
+
+# [Review]
+
+# 잘가 백준
+
+
+if __name__ == "__main__":
+    main()
