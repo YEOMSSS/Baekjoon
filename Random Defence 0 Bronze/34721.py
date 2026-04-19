fc56771dd672f352f864af13cfbcebcef6df3655
YEOM <junhayeom@gmail.com>
2026-04-19 20:47:04 +0900

    260419

---
+++ Random Defence 0 Bronze/34721.py
@@ -0,0 +1,39 @@
+# Authored by : marigold2003
+# Date : 2026-04-19
+# Link : https://www.acmicpc.net/problem/34721
+
+
+import sys
+
+input = sys.stdin.readline
+
+
+# [Summary] 역사를 걸으면 동국이 보이고
+
+# "I love DGU"를 N줄 출력하시오.
+
+
+def main() -> None:
+
+    # [Ideas]
+
+    # 내가 사랑한 것은 백준이건만
+
+    ##########
+
+    N = int(input())
+    for _ in range(N):
+        print("I love DGU")
+
+    ##########
+
+    return
+
+
+# [Review]
+
+# I love BOJ
+
+
+if __name__ == "__main__":
+    main()
