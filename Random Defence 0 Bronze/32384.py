b1b41031ee1af5424d77edb86cf374a1a91c03cf
YEOM <junhayeom@gmail.com>
2026-04-22 23:34:49 +0900

    260422

---
+++ Random Defence 0 Bronze/32384.py
@@ -0,0 +1,38 @@
+# Authored by : marigold2003
+# Date : 2026-04-22
+# Link : https://www.acmicpc.net/problem/32384
+
+
+import sys
+
+input = sys.stdin.readline
+
+
+# [Summary] 사랑은 고려대입니다
+
+# LoveisKoreaUniversity를 공백으로 구분하여 N번 출력한다.
+
+
+def main() -> None:
+
+    # [Ideas]
+
+    # 그래, 해봅시다
+
+    ##########
+
+    N = int(input())
+    print("LoveisKoreaUniversity " * N)
+
+    ##########
+
+    return
+
+
+# [Review]
+
+# 이제 며칠 남았나
+
+
+if __name__ == "__main__":
+    main()
